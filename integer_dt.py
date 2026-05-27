cf1cb247a8cefeaf200f4bc8483b5697da325395
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-27 11:54:12 +0530

    integers datatype

---
+++ integer_dt.py
@@ -0,0 +1,36 @@
+#decimal format
+batch_no=10
+print(batch_no)
+print(type(batch_no))
+
+#binary format
+emp_id=0b1001
+print(emp_id)
+print(type(emp_id))
+
+#octal format
+student_rollno=0o474
+print(student_rollno)
+print(type(student_rollno))
+
+#hexadecimal format
+account_id=0x19aec12
+print(account_id)
+print(type(account_id))
+
+#integer->binary,octal,hexadecimal
+
+system_no=573
+print("integer format of'system_no':",system_no)
+print("binary format of'system_no':",bin(system_no))
+
+system_id=447
+print("integer format of'system_id':",system_id)
+print("octal format of'system_id':",oct(system_id))
+
+book_no=388
+print("integer format of'book_no':",book_no)
+print("hexadecimal format of'book_no':",hex(book_no))
+
+
+
