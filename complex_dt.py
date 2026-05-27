a1f36fbc5e3b622b50cb6889092592aafe2cce68
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-27 12:18:05 +0530

    complex datatypes

---
+++ complex_dt.py
@@ -0,0 +1,24 @@
+data=3+4j
+print(data)
+print(type(data))
+
+data_1=3-4j
+print(data_1)
+print(type(data_1))
+
+data_2=-(-2-0j)
+print(data_2)
+print(type(data_2))
+
+data=2*6j
+print(data)
+print(type(data))
+
+data_3=0j     #0j , #if data_3=0+j,o/p:name error
+print(data_3)
+
+data_4=10/5j
+print(data_4)
+
+data_5=10//5j
+print(data_5)
